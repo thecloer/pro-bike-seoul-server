62e2b4932ea77dae094e1212bf02926a85b6ba01
cloer <chltjrrb97@gmail.com>
2023-04-11 10:19:57 +0900

    FIX: makeSeoulBikeIndexes sort ASC

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -3,19 +3,21 @@ export const makeSeoulBikeApiIndexes = (
   indexes: number[],
 ): { startIdx: number; endIdx: number }[] => {
   if (indexes.length < 1) return [];
-  return indexes.reduce(
-    (acc, curIdx, i) => {
-      if (i === 0) return acc;
-      const startIdx = acc.at(-1).startIdx;
-      const gap = curIdx - startIdx;
+  return indexes
+    .sort((a, b) => a - b)
+    .reduce(
+      (acc, curIdx, i) => {
+        if (i === 0) return acc;
+        const startIdx = acc.at(-1).startIdx;
+        const gap = curIdx - startIdx;
 
-      if (gap < SEOUL_BIKE_API_MAX_GAP) acc.at(-1).endIdx = curIdx;
-      else acc.push({ startIdx: curIdx, endIdx: curIdx });
+        if (gap < SEOUL_BIKE_API_MAX_GAP) acc.at(-1).endIdx = curIdx;
+        else acc.push({ startIdx: curIdx, endIdx: curIdx });
 
-      return acc;
-    },
-    [{ startIdx: indexes[0], endIdx: indexes[0] }],
-  );
+        return acc;
+      },
+      [{ startIdx: indexes[0], endIdx: indexes[0] }],
+    );
 };
 
 export const isFullFilled = <T>(
